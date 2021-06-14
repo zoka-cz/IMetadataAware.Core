c92d0eb9743f4c949b2d6a503a4ad2c592fed348
Michal Linhart <michal.linhart@gmail.com>
2021-06-14 10:51:38 +0200

    * first commit - before testing build

---
+++ IMetadataAware.Core/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace IMetadataAware.Core
+{
+	/// <summary>First test class</summary>
+	public class ClassTest
+	{
+		/// <summary>Test Value from Class test</summary>
+		public string										TestValue { get; set; } = "Test value from ClassTest";
+	}
+}
